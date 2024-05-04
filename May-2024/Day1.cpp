532cf99c3fe2245d2bd4990f70ed162c848c470e
INDERJEET <149118888+Inderjeet1Singh@users.noreply.github.com>
2024-05-05 03:53:44 +0530

    Reverse Prefix of Word Day1.cpp

--- May-2024/Day1.cpp
+++ May-2024/Day1.cpp
@@ -1,1 +1,16 @@
+/* Question Number 2000 on Leetcode here is Link https://leetcode.com/problems/reverse-prefix-of-word/?envType=daily-question&envId=2024-05-01 */
+//My Solution
 
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int i =0,j=0; //initialize two pointer
+        while( j<word.length() && word[j]!=ch){
+            j++; // moving j while it cannot move to given ch
+        }
+        while(i<=j && j<word.length()){
+            swap(word[i++],word[j--]); //swap
+        }
+        return word;
+    }
+};
