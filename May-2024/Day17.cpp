35550d8e55df038505cab6c1164258cd3f9f6230
INDERJEET <149118888+Inderjeet1Singh@users.noreply.github.com>
2024-05-29 23:04:52 +0530

    Delete Leaves With a Given Value Day17.cpp

---
+++ May-2024/Day17.cpp
@@ -0,0 +1,29 @@
+// Day17 17-05-24
+// Question --> Delete Leaves With a Given Value
+//Link --> https://leetcode.com/problems/delete-leaves-with-a-given-value/?envType=daily-question&envId=2024-05-17
+
+//Solution 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+         if(root==NULL) return root;
+        root->left = removeLeafNodes(root->left,target);
+        root->right = removeLeafNodes(root->right,target);
+        if(root->left==NULL && root->right==NULL && root->val==target){
+            return NULL;
+        }
+        return root;
+    }
+};
