8c61e0dd6cb0463fae82e3502ee18d9660fb155e
INDERJEET <149118888+Inderjeet1Singh@users.noreply.github.com>
2024-06-02 22:25:30 +0530

    Reverse String Day2.cpp

--- June-2024/Day2.cpp
+++ June-2024/Day2.cpp
@@ -1,1 +1,14 @@
+// Day2
+// Reverse String
 
+// Solution
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i=0,j=s.size()-1; // two pointer i = point to start j = point to the end of string 
+        while(i<=j){
+            swap(s[i++],s[j--]); // swaping the last pointer value with start pointer and increment the start pointer and decrement the end pointer
+        }
+    }
+};
