2240c0a1be2acb11da13abc57b2616ab428c0a7a
INDERJEET <149118888+Inderjeet1Singh@users.noreply.github.com>
2024-11-02 19:49:04 +0530

    2490. Circular Sentence

---
+++ Nov/Day2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int j=0;
+        while(j<sentence.length()){
+            while(j<sentence.length() && sentence[j]!=' ') j++;
+            if(j==sentence.length()){
+                if(sentence[j-1]!=sentence[0]) return false;
+            }
+            else
+                if(sentence[j-1]!=sentence[j+1]) return false;
+                j++;
+        }
+        return true;
+    }
+};
