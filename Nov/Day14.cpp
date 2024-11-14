b23b2603aff6dff7deb4cd45dc117d99a35fafee
INDERJEET <149118888+Inderjeet1Singh@users.noreply.github.com>
2024-11-15 03:25:48 +0530

    Code Testcase Testcase Test Result 2064. Minimized Maximum of Products Distributed to Any Store

---
+++ Nov/Day14.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int start = 1,end = 0;
+        for(auto e : quantities){
+            if(end<e){
+                end = e;
+            }
+        }
+        int mid = 0 , sum = 0,ans = 0;
+        while(start<=end){
+             mid = start + (end - start)/2;
+            sum = 0;
+            for(int i=0;i<quantities.size();i++){
+                sum += (quantities[i] + mid -1)/mid; 
+            }
+            if(sum<=n){
+                ans = mid;
+                end = mid-1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
